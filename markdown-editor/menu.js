223e5ca2bc9b0d24730ae8a43c9acd2bb4ddae16
kronosGR <geo.elgeo@gmail.com>
2024-03-29 08:28:31 +0100

    added the ready component for markdown

---
+++ markdown-editor/menu.js
@@ -0,0 +1,36 @@
+const { Menu, shell, app } = require('electron');
+
+const template = [
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'About Editor Component',
+        click() {
+          shell.openExternal('https://simplemde.com');
+        },
+      },
+    ],
+  },
+  {
+    label: 'Debugging',
+    submenu: [
+      {
+        label: 'Dev Tools',
+        role: 'toggleDevTools',
+      },
+      { type: 'separator' },
+      { role: 'reload', accelerator: 'Alt+R' },
+    ],
+  },
+];
+
+if (process.platform === 'darwin') {
+  template.unshift({
+    label: app.getName(),
+    submenu: [{ role: 'about' }, { type: 'separator' }, { role: 'quit' }],
+  });
+}
+
+const menu = Menu.buildFromTemplate(template);
+module.exports = menu;
