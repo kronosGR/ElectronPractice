72c6a3f4285204979940b395de067f91e349e932
kronosGR <geo.elgeo@gmail.com>
2024-03-30 12:46:36 +0100

    tested components and routing

--- electron-with-react/src/App.js
+++ electron-with-react/src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
+import { Navbar, Button, Alignment } from '@blueprintjs/core';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
+
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function Files() {
+  return <h2>Files</h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <BrowserRouter>
+      <Navbar className='bp3-dark'>
+        <Navbar.Group align={Alignment.LEFT}>
+          <Navbar.Heading>Blueprint</Navbar.Heading>
+          <Navbar.Divider />
+          <Link className='bp3-button bp3-minimal bp3-icon-home' to='/'>
+            Home
+          </Link>
+          <Link className='bp3-button bp3-minimal bp3-icon-document' to='/files/'>
+            Files
+          </Link>
+        </Navbar.Group>
+      </Navbar>
+
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <Routes>
+          <Route path='/' exact element={<Index />} />
+          <Route path='/files' element={<Files />} />
+        </Routes>
       </header>
-    </div>
+    </BrowserRouter>
   );
 }
 
