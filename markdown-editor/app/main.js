c84a86acf4ec95f94523f567698af4bcba5e4770
kronosGR <geo.elgeo@gmail.com>
2024-04-02 10:27:26 +0200

    added load file

--- markdown-editor/app/main.js
+++ markdown-editor/app/main.js
@@ -1,12 +1,51 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog, webContents } = require('electron');
+const fs = require('fs');
+
+require('@electron/remote/main').initialize();
 
 let mainWindow = null;
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow();
+  mainWindow = new BrowserWindow({
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+  });
+  require('@electron/remote/main').enable(mainWindow.webContents);
   mainWindow.loadFile('./app/index.html');
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+    //getFileFromUser();
+    mainWindow.webContents.openDevTools();
+  });
 
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
 });
+
+const getFileFromUser = (exports.getFileFromUser = async () => {
+  const files = await dialog.showOpenDialog(mainWindow, {
+    properties: ['openFile'],
+    filters: [
+      { name: 'Markdown files', extensions: ['md', 'markdown'] },
+      {
+        name: 'Text files',
+        extensions: ['txt'],
+      },
+    ],
+  });
+
+  if (files) {
+    const file = files.filePaths[0];
+    openFile(file);
+  }
+});
+
+const openFile = (file) => {
+  const content = fs.readFileSync(file).toString();
+  mainWindow.webContents.send('file-opened', file, content);
+};
---
+++ markdown-editor/app/renderer.js
@@ -0,0 +1,32 @@
+const { marked } = require('marked');
+const mainProcess = require('@electron/remote').require('./main');
+const { ipcRenderer } = require('electron');
+
+const markdownView = document.querySelector('#markdown');
+const htmlView = document.querySelector('#html');
+const newFileButton = document.querySelector('#new-file');
+const openFileButton = document.querySelector('#open-file');
+const saveMarkdownButton = document.querySelector('#save-markdown');
+const revertButton = document.querySelector('#revert');
+const saveHtmlButton = document.querySelector('#save-html');
+const showFileButton = document.querySelector('#show-file');
+const openInDefaultButton = document.querySelector('#open-in-default');
+
+markdownView.addEventListener('keyup', (event) => {
+  const currentContent = event.target.value;
+  console.log(currentContent);
+  renderMarkdownToHTML(currentContent);
+});
+
+openFileButton.addEventListener('click', () => {
+  mainProcess.getFileFromUser();
+});
+
+const renderMarkdownToHTML = (markdown) => {
+  htmlView.innerHTML = marked(markdown, { sanitize: true });
+};
+
+ipcRenderer.on('file-opened', (event, file, content) => {
+  markdownView.value = content;
+  renderMarkdownToHTML(content);
+});
