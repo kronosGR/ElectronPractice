0313d18015537308fbbdfa3d75917c0cdd534ce2
kronosGR <geo.elgeo@gmail.com>
2024-04-04 09:05:29 +0200

    added check for dark mode and diff icon

--- ClipboardMaster/app/main.js
+++ ClipboardMaster/app/main.js
@@ -1,11 +1,19 @@
-const { app, Menu, Tray } = require('electron');
+const { app, Menu, Tray, nativeTheme } = require('electron');
 
 const path = require('path');
 
 let tray = null;
 
+const getIcon = () => {
+  if (process.platform === 'win32') return 'icon-light@2x.ico';
+  if (nativeTheme.shouldUseDarkColors) return 'Icon-light.png';
+  return 'icon-dark.png';
+};
+
 app.on('ready', () => {
-  tray = new Tray(path.join(__dirname, '/icon-dark.png'));
+  if (app.dock) app.dock.hide();
+
+  tray = new Tray(path.join(__dirname, '../icons/', getIcon()));
 
   if (process.platform === 'win32') {
     tray.on('click', tray.popUpContextMenu);
