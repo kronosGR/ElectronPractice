3bc729e1c62b9a3e149c075df166de57cfb75937
kronosGR <geo.elgeo@gmail.com>
2024-04-04 12:59:08 +0200

    add from clipboard

--- clipboardMaster2/app/main.js
+++ clipboardMaster2/app/main.js
@@ -1,9 +1,16 @@
 const { menubar } = require('menubar');
 
-const menuBar = menubar({ index: `file://${__dirname}/index.html` });
-
-menuBar.on('ready', () => {
-  console.log('sss');
+const menuBar = menubar({
+  index: `file://${__dirname}/index.html`,
+  browserWindow: {
+    webPreferences: {
+      enableRemoteModule: true,
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  },
 });
+
+menuBar.on('ready', () => {});
 
 menuBar.on('after-create-window', () => {});
