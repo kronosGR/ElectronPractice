e2c5e5395642989252a4f24938f229c61b84a2f3
kronosGR <geo.elgeo@gmail.com>
2024-04-05 10:58:44 +0200

    added global shortcuts

--- clipboardMaster2/app/main.js
+++ clipboardMaster2/app/main.js
@@ -1,6 +1,8 @@
 const { menubar } = require('menubar');
+const { globalShortcut, Menu } = require('electron');
 
 const menuBar = menubar({
+  preloadWindow: true,
   index: `file://${__dirname}/index.html`,
   browserWindow: {
     webPreferences: {
@@ -11,6 +13,43 @@ const menuBar = menubar({
   },
 });
 
-menuBar.on('ready', () => {});
+menuBar.on('ready', () => {
+  const secondaryMenu = Menu.buildFromTemplate([
+    {
+      label: 'Quit',
+      click() {
+        menuBar.app.quit();
+      },
+      accelerator: 'CommandOrControl+Q',
+    },
+  ]);
+  menuBar.tray.on('right-click', () => {
+    console.log('aaa');
+    menuBar.tray.popUpContextMenu(secondaryMenu);
+  });
+
+  const createClipping = globalShortcut.register('Alt+1', () => {
+    menuBar.window.webContents.send('create-new-clipping');
+  });
+
+  const writeClipping = globalShortcut.register('CmdOrCtrl+Alt+1', () => {
+    menuBar.window.webContents.send('write-to-clipboard');
+  });
+
+  const publishClipping = globalShortcut.register('Alt+2', () => {
+    menuBar.window.webContents.send('publish-clipping');
+  });
+
+  if (!createClipping) {
+    console.log('Registration failed', 'createClipping');
+  }
+
+  if (!writeClipping) {
+    console.error('Registration failed', 'writeClipping');
+  }
+  if (!publishClipping) {
+    console.error('Registration failed', 'publishClipping');
+  }
+});
 
 menuBar.on('after-create-window', () => {});
--- clipboardMaster2/app/renderer.js
+++ clipboardMaster2/app/renderer.js
@@ -1,6 +1,28 @@
-const { clipboard } = require('electron');
+const { clipboard, shell, ipcRenderer } = require('electron');
 const clippingsList = document.getElementById('clippings-list');
 const copyFromClipboardButton = document.getElementById('copy-from-clipboard');
+
+const request = require('request').defaults({
+  url: 'https://api.github.com/gists',
+  headers: { 'User-Agent': 'Clipboard master 2' },
+});
+
+ipcRenderer.on('create-new-clipping', () => {
+  addClippingToList();
+  new Notification('Clipping Added', {
+    body: `${clipboard.readText()}`,
+  });
+});
+
+ipcRenderer.on('write-to-clipboard', () => {
+  const clipping = clippingsList.firstChild;
+  writeToClipBoard(getClippingText(clipping));
+});
+
+ipcRenderer.on('publish-clipping', () => {
+  const clipping = clippingsList.firstChild;
+  publishClipping(getClippingText(clipping));
+});
 
 const createClippingElement = (clippingText) => {
   const clippingElement = document.createElement('article');
@@ -20,6 +42,18 @@ const createClippingElement = (clippingText) => {
   return clippingElement;
 };
 
+const removeClipping = (target) => {
+  target.remove();
+};
+
+const getButtonParent = ({ target }) => {
+  return target.parentNode.parentNode;
+};
+
+const getClippingText = (clippingListItem) => {
+  return clippingListItem.querySelector('.clipping-text').innerText;
+};
+
 const addClippingToList = () => {
   console.log('added');
   const clippingText = clipboard.readText();
@@ -27,4 +61,37 @@ const addClippingToList = () => {
   clippingsList.prepend(clippingElement);
 };
 
+const writeToClipBoard = (clippingText) => {
+  clipboard.writeText(clippingText);
+};
+
+const publishClipping = (clipping) => {
+  request.post({ json: { clipping } }),
+    (error, response, body) => {
+      if (error) {
+        return new Notification('Error publishing your clipping', {
+          body: JSON.parse(error).message,
+        });
+      }
+
+      const url = body.url;
+      const notification = new Notification('Your clipping has been publish', {
+        body: `Click to open ${url} in your browser`,
+      });
+      notification.onclick = () => {
+        shell.openExternal(url);
+      };
+      clipboard.writeText(url);
+    };
+};
+
 copyFromClipboardButton.addEventListener('click', addClippingToList);
+
+clippingsList.addEventListener('click', (event) => {
+  const hasClass = (className) => event.target.classList.contains(className);
+  const clippingListItem = getButtonParent(event);
+
+  if (hasClass('remove-clipping')) removeClipping(clippingListItem);
+  if (hasClass('copy-clipping')) writeToClipBoard(getClippingText(clippingListItem));
+  if (hasClass('publish-clipping')) publishClipping(getClippingText(clippingListItem));
+});
